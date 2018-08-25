1c98bdc8dfeb28b6c0b4db34b486ccc1a48c13bf
Flávio Sousa <fn_maia@outlook.com>
2018-08-25 20:20:09 -0300

    Added external operators

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "vetor.cc"
 
+using namespace std;
+
 int main() {
-	Vector<3, int> v1, v2;
+	Vector<3, int> v1, v2, v3;
 	v1 = 5, 4, 3;
 	v2 = 1, 5, 2;
-	v1.printArray();
+	/*v1.printArray();
 	v2.printArray();
 	v1.add(v2);
 	v1.printArray();
@@ -22,5 +24,11 @@ int main() {
 	2 * v2;
 	v2.printArray();
 	std::cout << v1 * v2 << std::endl;
-	std::cout << v2.dotProduct(v2) << std::endl;
+	std::cout << v2.dotProduct(v2) << std::endl;*/
+	cout << v1 << endl;
+	cout << v2 << endl;
+	v3 = v1 ** v2;
+	cout << v3 << endl;
+	v3 = crossProduct(v1, v2);
+	cout << v3 << endl;
 }
--- src/vetor.cc
+++ src/vetor.cc
@@ -1,6 +1,12 @@
 #include <iostream>
 
 template <int D, typename T>
+class Vector;
+
+template <typename T>
+Vector<3, T> crossProduct(const Vector<3, T>& u, const Vector<3, T>& v);
+
+template <int D, typename T>
 class Vector {
 	public:
 		Vector();
@@ -25,6 +31,9 @@ class Vector {
 		Vector& operator /(T n);
 		T dotProduct(const Vector& v);
 		T operator *(const Vector& v);
+		template <typename _T>
+		friend Vector<3, _T> crossProduct(const Vector<3, _T>& u, const Vector<3, _T>& v);
+		void print(decltype(std::cout)& ostream) const;
 
 		void printArray(); //Temporary
 		void printSize(); //Temporary
@@ -33,11 +42,6 @@ class Vector {
 		T array[D];
 		int size;
 };
-
-template <int D, typename T>
-Vector<D, T>& operator *(T n, Vector<D, T>& v) {
-	return v * n;
-}
 
 template <int D, typename T>
 Vector<D, T>::Vector(): size(0) {
@@ -147,6 +151,71 @@ T Vector<D, T>::dotProduct(const Vector& v) {
 template <int D, typename T>
 T Vector<D, T>::operator *(const Vector& v) {
 	return dotProduct(v);
+}
+
+template <int D, typename T>
+void Vector<D, T>::print(decltype(std::cout)& ostream) const {
+	for (int i = 0; i < D - 1; i++)
+		ostream << array[i] << ' ';
+	ostream << array[D - 1];
+}
+
+template <int D, typename T>
+Vector<D, T>& operator *(T n, Vector<D, T>& v) {
+	return v * n;
+}
+
+template <typename T>
+class VectorCrossOperatorMediator;
+
+template <typename T>
+VectorCrossOperatorMediator<T> operator *(const Vector<3, T>& v);
+
+template <typename T>
+Vector<3, T> operator *(const Vector<3, T>& v, const VectorCrossOperatorMediator<T>& m);
+
+template <typename T>
+class VectorCrossOperatorMediator {
+	private:
+		const Vector<3, T>& v;
+		template <typename _T>
+		friend Vector<3, _T> operator *(const Vector<3, _T>& v, const VectorCrossOperatorMediator<_T>& m);
+
+		VectorCrossOperatorMediator(const Vector<3, T>& callerV);
+		template <typename _T>
+		friend VectorCrossOperatorMediator<_T> operator *(const Vector<3, _T>& v);
+};
+
+template <typename T>
+VectorCrossOperatorMediator<T>::VectorCrossOperatorMediator(const Vector<3, T>& callerV): v (callerV) {}
+
+template <typename T>
+VectorCrossOperatorMediator<T> operator *(const Vector<3, T>& v) {
+	return VectorCrossOperatorMediator<T>(v);
+}
+
+template <typename T>
+Vector<3, T> crossProduct(const Vector<3, T>& u, const Vector<3, T>& v) {
+	T r1 = u.array[1]*v.array[2] - u.array[2]*u.array[1];
+	T r2 = u.array[2]*v.array[0] - u.array[0]*v.array[2];
+	T r3 = u.array[0]*v.array[1] - u.array[1]*v.array[0];
+
+	Vector<3, T> result;
+	result = r1, r2, r3;
+
+	return result;
+}
+
+template <typename T>
+Vector<3, T> operator *(const Vector<3, T>& v, const VectorCrossOperatorMediator<T>& m) {
+	return crossProduct(v, m.v);
+}
+
+template <int D, typename T>
+decltype(std::cout)& operator <<(decltype(std::cout)& cout, const Vector<D, T>& v) {
+	v.print(cout);
+
+	return cout;
 }
 
 template <int D, typename T>
