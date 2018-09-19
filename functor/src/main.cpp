47ed7c055485899a2357eb0ca41064858d833fab
Flávio Sousa <fn_maia@outlook.com>
2018-09-19 00:48:21 -0300

    Re-added parenthesis to power string

--- functor/src/main.cpp
+++ functor/src/main.cpp
@@ -8,9 +8,13 @@ int main() {
 
 	//auto f = sin(exp(x) / 3.0) + cos(x) - x->*3 * log(x) * 5.0;
 
-	auto f = sin(3.0) - exp(cos(x)) * x->*4 / 4.0 + log(7.0);
+	//auto f = sin(3.0) - exp(cos(x)) * x->*4 / 4.0 + log(7.0);
 
-	cout << f(5.3) << ' ' << f.dx(5.3) << endl;
+	auto f =  1.0 / (sin(x)->*2 + cos(x)->*2)->*4;
 
-	cout << f.str() << endl << f.dx_str() << endl;
+	//cout << f(5.3) << ' ' << f.dx(5.3) << endl;
+
+	cout << "f(x) = " << f.str() << ", f'(x) = " << f.dx_str() << endl << endl;
+
+	cout << "f(x) = ((1)/(((((sin(x))^2)+((cos(x))^2)))^4)), f'(x) = (((0)*(((((sin(x))^2)+((cos(x))^2)))^4)-(1)*((4*((((2*((cos(x)*1))*(sin(x))^1))+((2*((-sin(x)*1))*(cos(x))^1))))*((((sin(x))^2)+((cos(x))^2)))^3)))/(((((sin(x))^2)+((cos(x))^2)))^4)^2)" << endl;
 }
