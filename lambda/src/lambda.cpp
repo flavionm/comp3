2fc54fe677a6e8d281924f2caf6e215ab55d4739
Flávio Sousa <fn_maia@outlook.com>
2018-09-26 14:21:18 -0300

    Created array_size function

--- lambda/src/lambda.cpp
+++ lambda/src/lambda.cpp
@@ -3,3 +3,8 @@ void operator | (V iterable, F function) {
 	for (auto x : iterable)
 		function(x);
 }
+
+template<typename T, int S>
+int array_size(T(& t)[S]) {
+	return S;
+}
--- lambda/src/main.cpp
+++ lambda/src/main.cpp
@@ -5,6 +5,9 @@
 using namespace std;
 
 int main() {
+	int array[] = {1, 5, 3, 5, 7, 8};
+	cout << array_size(array) << endl << endl;
+
 	vector<int> v = {1, 5, 3, 5, 7, 8};
 	v | [] (int x) {cout << x << endl;};
 
