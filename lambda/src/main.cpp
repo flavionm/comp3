cbe03b52ff1aeca9b9658967e1dc9a61686490af
Flávio Sousa <fn_maia@outlook.com>
2018-09-26 14:38:35 -0300

    Added function pointer test to main.cpp

--- lambda/src/main.cpp
+++ lambda/src/main.cpp
@@ -4,13 +4,20 @@
 
 using namespace std;
 
+void f (int x) {
+	cout << x*x << endl;
+}
+
 int main() {
 	int a[] = {1, 5, 3, 5, 7, 8};
 	vector<int> v = {1, 5, 3, 5, 7, 8};
 
 	v | [] (int x) {cout << x << endl;};
 	cout << endl;
+	v | f;
+	cout << endl;
 	a | [] (int x) {cout << x << endl;};
+	cout << endl;
 
 	return 0;
 }
