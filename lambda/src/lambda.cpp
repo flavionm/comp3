30f881fca8395220ec0911d52469c5915e6e2180
Flávio Sousa <fn_maia@outlook.com>
2018-09-29 16:43:11 -0300

    Implemented filter to arrays

--- lambda/src/lambda.cpp
+++ lambda/src/lambda.cpp
@@ -1,4 +1,4 @@
-#include <typeinfo>
+#include <vector>
 #include <type_traits>
 
 template <typename V, typename F>
@@ -16,7 +16,15 @@ auto operator | (V iterable, F function) -> typename std::enable_if<std::is_same
 }
 
 template <typename T, int S, typename F>
-void operator | (T(& array)[S], F function) {
+auto operator | (T(& array)[S] , F function) -> typename std::enable_if<std::is_same<decltype(function(array[0])), bool>::value, std::vector<T>>::type {
+	std::vector<T> filtered;
+	for (int i = 0; i < S; i++)
+		if (function(array[i])) filtered.push_back(array[i]);
+	return filtered;
+}
+
+template <typename T, int S, typename F>
+auto operator | (T(& array)[S], F function) -> typename std::enable_if<std::is_same<decltype(function(array[0])), void>::value, void>::type {
 	for (int i = 0; i < S; i++)
 		function(array[i]);
 }
--- lambda/src/main.cpp
+++ lambda/src/main.cpp
@@ -8,15 +8,23 @@ void f (int x) {
 	cout << x*x << endl;
 }
 
+bool fi (int x) {
+	return x % 2 == 1;
+}
+
 int main() {
 	int a[] = {1, 2, 3, 4, 7, 8};
 	vector<int> v = {1, 2, 3, 4, 7, 8};
 
 	v | [] (int x) {return x % 2 == 0;} | [] (int x) {cout << x << endl;};
 	cout << endl;
-	v | f;
+	v | fi | f;
 	cout << endl;
-	a | [] (int x) {cout << x << endl;};
+	v | [] (int x) {return x % 2 == 0;} | f;
+	cout << endl;
+	v | fi | [] (int x) {cout << x << endl;};
+	cout << endl;
+	a | [] (int x) {return x % 2 == 0;} | [] (int x) {cout << x << endl;};
 	cout << endl;
 
 	return 0;
