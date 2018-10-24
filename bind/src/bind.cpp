532c8c6516668f529e5c3a12cbc2bf67e116ecf8
Flávio Sousa <fn_maia@outlook.com>
2018-10-24 20:13:41 -0300

    Added operator parenthesis

--- bind/src/bind.cpp
+++ bind/src/bind.cpp
@@ -4,7 +4,13 @@ template <typename F, typename... A>
 class Bind {
 	public:
 		Bind(F f, A... args): f(f), args(args...) {}
-	
+		template <typename... R>
+		auto operator () (R... rem) {
+			std::tuple<R...> t_rem (rem...);
+			auto t = std::tuple_cat(args, t_rem);
+			return std::apply(f, t);
+		}
+
 	private:
 		F f;
 		std::tuple<A...> args;
--- bind/src/main.cpp
+++ bind/src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "bind.cpp"
 
-void func(int a) {}
+using namespace std;
+
+void func(int a, double b) {
+	cout << a << " " << b << endl;
+}
 
 int main() {
 	auto f = bind(func, 5);
+
+	f(4.5);
 
 	return 0;
 }
